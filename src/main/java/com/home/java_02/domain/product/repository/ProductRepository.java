abe68ebd80c89c280541615b031126275f2cc28d
JeongHoon.Han <yuria2005@gmail.com>
2025-07-02 22:59:54 +0900

    feature: 주문 취소 구현 → 리팩토링
    클린코드와 리팩토링
    
    메인 서비스와 하위 서비스 관계 유지 (퍼사드 패턴)
    하위 서비스는 단일 기능 (단일 책임 원칙)

--- src/main/java/com/home/java_02/domain/product/repository/ProductRepository.java
+++ src/main/java/com/home/java_02/domain/product/repository/ProductRepository.java
@@ -1,12 +1,11 @@
 package com.home.java_02.domain.product.repository;
 
 import com.home.java_02.domain.product.entity.Product;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-  List<Product> findByIdIn(List<Long> in);
+  //List<Product> findByIdIn(List<Long> in);//확인 필요
 }
